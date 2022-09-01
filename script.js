e1df4cb8d806f5cef11308da5b0511da88cf24bc
Hamouche Hamid <skilled.dev@hotmail.com>
2022-09-01 16:14:27 +0100

    projet init

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    // variables
+    $surface = $('.surface');
+    $car = $('.car');
+    $img = $('.car Img');
+    let flag = true;
+
+    const cars = ['./assets/Img_05.png','./assets/Img_06.png'];
+
+    // keypress event
+    $(document).on('keypress', function(e){
+        if(e.which == 13){
+            $($surface).toggleClass('moveRight');
+            $($car).toggleClass('suspension');
+        }
+      
+    })
+
+    //lights
+    $(document).on('keypress', function(e){
+        if(e.which == 119){
+            if(flag){
+               flag = flase;
+               $img.attr('src',cars[0]);
+
+            }else{
+                flag = true;
+                $img.attr('src',cars[1]);
+            }
+        }
+      
+    })
+
+});
